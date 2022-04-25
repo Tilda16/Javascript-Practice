7080d9fa7566286371df55ea7b946bc1203dc77c
Tilda16 <rebekkakruck@gmail.com>
2022-04-25 11:38:05 +0200

    Did it for the second dice as well

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -5,4 +5,4 @@ let changedImage1 = "images/dice" + randomNumber1 + ".png";
 let changedImage2 = "images/dice" + randomNumber1 + ".png";
 
 document.querySelector(".dice .img1").setAttribute("src",changedImage1);
-document.querySelector(".dice .img1").setAttribute("src",changedImage2);
+document.querySelector(".dice .img2").setAttribute("src",changedImage2);
