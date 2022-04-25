ca42487e71a8659691cb89af047e367f26c0bb61
Tilda16 <rebekkakruck@gmail.com>
2022-04-25 11:54:40 +0200

    Made a javascript function to select a winner

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -6,3 +6,17 @@ let changedImage2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".dice .img1").setAttribute("src",changedImage1);
 document.querySelector(".dice .img2").setAttribute("src",changedImage2);
+
+function winner() {
+  if(randomNumber1 > randomNumber2){
+    document.selectElementByClassName(".title").setAttribute("h1", "Player 1 Wins! Hooray!");
+  }
+  else if (randomNumber1 === randomNumber2){
+    document.selectElementByClassName(".title").setAttribute("h1", "It's a draw!");
+  }
+else {
+  document.selectElementByClassName(".title").setAttribute("h1", "Player 2 Wins! Hooray!");
+}
+}
+
+winner();
