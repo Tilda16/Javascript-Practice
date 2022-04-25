068166bdf42922c9b90c3c38066679230f199d79
Tilda16 <rebekkakruck@gmail.com>
2022-04-25 11:38:50 +0200

    Fixed the issue with the same random number

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -2,7 +2,7 @@ let randomNumber1 = Math.floor(Math.random()*6)+1;
 let randomNumber2 = Math.floor(Math.random()*6)+1;
 
 let changedImage1 = "images/dice" + randomNumber1 + ".png";
-let changedImage2 = "images/dice" + randomNumber1 + ".png";
+let changedImage2 = "images/dice" + randomNumber2 + ".png";
 
 document.querySelector(".dice .img1").setAttribute("src",changedImage1);
 document.querySelector(".dice .img2").setAttribute("src",changedImage2);
